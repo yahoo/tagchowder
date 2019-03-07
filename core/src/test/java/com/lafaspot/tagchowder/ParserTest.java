dae8b73e997ecc066efd4421c53ff51cfa6d5e97
Deeksha Vasisth <deekshav@yahoo-inc.com>
2019-03-06 18:44:33 -0800

    Add copyright information to code files

--- core/src/test/java/com/lafaspot/tagchowder/ParserTest.java
+++ core/src/test/java/com/lafaspot/tagchowder/ParserTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *  ====================================================================
+ * Changes to the original project are Copyright 2019 Oath Inc.
  */
 
 package com.lafaspot.tagchowder;
