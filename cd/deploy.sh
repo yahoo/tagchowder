905cd3004193394b2213829b279bb791a234ce20
Fan Su <fansu.dev@gmail.com>
2019-05-09 17:18:34 -0700

    remove redundant command

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P build-extras --settings cd/mvnsettings.xml
+    mvn deploy --settings cd/mvnsettings.xml
 fi
