ebe293e3a8ddd88f9f0edeb6b87332a1c8f1c420
Manish Singh <manishsingh@yahoo-inc.com>
2019-11-22 17:59:09 -0800

    more changes again

--- core/templates/HTMLScanner.java
+++ core/templates/HTMLScanner.java
@@ -17,24 +17,6 @@
 
 package com.yahoo.tagchowder.templates;
 
-package com.yahoo.ymail.xmas.appbase.service.quotedtext.processors;
-
-/*
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *  ====================================================================
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -55,7 +37,7 @@ import com.yahoo.tagchowder.Scanner;
  * object to fetch characters from and a ScanHandler object to report lexical events to.
  */
 
-public class CustomScanner implements Scanner, Locator {
+public class HTMLScanner implements Scanner, Locator {
 
     // Start of state table
     private static final int S_ANAME = 1;
